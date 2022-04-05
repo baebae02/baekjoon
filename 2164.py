283a01e2a77bb5074c09828761f36d9d56bdb611
Seo Hyeon Bae <76519535+bread4614@users.noreply.github.com>
2022-04-05 17:26:52 +0900

    Update 2164.py

--- 2164.py
+++ 2164.py
@@ -1,1 +1,13 @@
-
+import sys
+from collections import deque 
+n = int(sys.stdin.readline().rstrip())
+q = deque([])
+for i in range(n):
+    q.append(n-i)
+while True:
+    if len(q) == 1:
+        break
+    del(q[-1])
+    q.appendleft(q[-1])
+    del(q[-1])
+print(q[0])
